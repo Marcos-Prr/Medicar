2fac3b9d2165c033444d0f185d53740ad56e7bde
Marcos-Prr <marcosprr34@gmail.com>
2021-05-14 17:55:15 -0300

    adicionando pacote markdown e django-filter

--- MedicarApp/MedicarApp/settings.py
+++ MedicarApp/MedicarApp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #apps
-    'medico.apps.MedicoConfig',
+    'medico',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -126,3 +126,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
