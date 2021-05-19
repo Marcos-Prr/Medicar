f0e7b6855ca52755f9f8b913b015ad814209a66d
Marcos-Prr <marcosprr34@gmail.com>
2021-05-19 18:21:18 -0300

    adicionado filtro de pesquisa para medicos

--- MedicarApp/medico/views.py
+++ MedicarApp/medico/views.py
@@ -2,17 +2,23 @@ from django.db.models import query
 from django.db.models.query_utils import refs_expression
 from rest_framework import serializers
 from rest_framework import views
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import medico,Especialidade
 from .serializers import medicoSerializer,EspecialidadeSerializer
 from rest_framework import viewsets
 
-class medicoViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = medico.objects.all()
-    serializer_class = medicoSerializer
-
 
 class EspecialidadeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Especialidade.objects.all()
     serializer_class = EspecialidadeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
+class medicoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = medico.objects.all()
+    serializer_class = medicoSerializer
+    filter_backends = [filters.SearchFilter,DjangoFilterBackend]
+    search_fields = ['nome']
+    filterset_fields = ['especialidade']
