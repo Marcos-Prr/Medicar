9351c02a1279a8e431c0d7cc0721a0a6a873e00d
Marcos-Prr <marcosprr34@gmail.com>
2021-05-19 18:20:56 -0300

    conserto na funcao create e adição da funcao delete

--- MedicarApp/consulta/views.py
+++ MedicarApp/consulta/views.py
@@ -1,9 +1,11 @@
+
 from rest_framework import serializers
 from rest_framework import views
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import consulta
-from .serializers import consultaSerializer
+from agenda.models import Horario
+from .serializers import consultaDestroySerializer, consultaSerializer ,consultaPostSerializer
 
 class consultaViewSet(viewsets.ViewSet):
 
@@ -14,7 +16,21 @@ class consultaViewSet(viewsets.ViewSet):
         return Response(serializer_class.data)
 
     def create(self,request):
-        serializersPost = consultaSerializer(data=request.data)
-        serializersPost.is_valid(raise_exception=True)
-        serializersPost.save()
-        return Response(serializersPost.data,status=status.HTTP_201_CREATED)
+        serializer = consultaPostSerializer(data=request.data,context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self,request,pk):
+        data={}
+        data['consulta_id']= pk
+        serializer = consultaDestroySerializer(data=data,context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        
+        consulta_id = pk
+        Consulta = consulta.objects.get(pk=consulta_id,usuario__username=request.user)
+        desmarcado = Horario.objects.filter(pk=Consulta.horario.id).update(marcado=False)
+        Consulta.delete()
+
+        data={}
+        return Response(data)
