bcedb32401983018ff1c935b04426e0d0b588151
Marcos-Prr <marcosprr34@gmail.com>
2021-05-21 00:42:03 -0300

    retaforação

--- app/MedicarApp/settings.py
+++ app/MedicarApp/settings.py
@@ -140,7 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #DRF
 REST_FRAMEWORK ={
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':(
