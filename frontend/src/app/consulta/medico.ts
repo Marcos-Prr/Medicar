5130220f9f03cdd47cccee959d6a0bddccdddfe3
MarcosPrr <marcosprr34@gmail.com>
2021-10-03 16:23:58 -0300

    Adicionado  variaveis nas interfaces  principais

--- frontend/src/app/consulta/medico.ts
+++ frontend/src/app/consulta/medico.ts
@@ -1,2 +1,10 @@
+import { Especialidade } from "./especialidade";
+
 export interface Medico {
+    id:string
+    crm:number
+    nome:string
+    email:string
+    telefone:string
+    especialidade: Especialidade
 }
