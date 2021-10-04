9671b094cbbf5bc7e44ef73dfaee2d67f8dca227
MarcosPrr <marcosprr34@gmail.com>
2021-10-04 17:47:22 -0300

    implementado listar consultas

--- frontend/src/app/consulta/read/read.component.ts
+++ frontend/src/app/consulta/read/read.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConsultaService } from '../consulta.service';
+import { ConsultaItem } from '../consulta-item';
+import { Consulta } from '../consulta';
 
 @Component({
   selector: 'app-read',
@@ -6,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadComponent implements OnInit {
 
-  constructor() { }
+  consultas: ConsultaItem[] = []
+  displayedColumns = ['especialidade', 'profissional','data','hora','action'];
+
+  constructor(private consultaService: ConsultaService,) { }
 
   ngOnInit(): void {
+    this.getConsultasList()
+    console.log(this.consultas);
+  }
+
+  private getConsultasList(): void {
+    this.consultaService.list().subscribe(
+      (consultas) => {
+        this.consultas = consultas.map((consulta) => this.consultaToItem(consulta))
+      }
+    )
+  }
+
+  private consultaToItem(consulta:Consulta):ConsultaItem{
+    return {
+              id: consulta.id,
+              data: consulta.dia,
+              especialidade: consulta.medico.especialidade.nome,
+              hora: consulta.horario,
+              profissional: consulta.medico.nome
+           }
   }
 
 }
