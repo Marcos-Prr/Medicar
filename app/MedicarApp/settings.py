50841c52a7134218c22411d1bad819c5f64d62f6
MarcosPrr <marcosprr34@gmail.com>
2021-10-04 16:55:43 -0300

    adicionado CORSheaders no backend para request de consultas

--- app/MedicarApp/settings.py
+++ app/MedicarApp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     'medico',
     'agenda',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +63,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+)
+
 
 ROOT_URLCONF = 'MedicarApp.urls'
 
