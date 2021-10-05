2a928bc3e732f9504ed710b23ea2e6edba52c24a
MarcosPrr <marcosprr34@gmail.com>
2021-10-05 01:06:35 -0300

    implementando login do usuario

--- frontend/src/app/user/login/login.component.ts
+++ frontend/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { UserLogin } from '../user-login';
+import { AuthService } from 'src/app/authenticate/authenticate.service';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  hide = true;
+
+  hidePassword():void{
+    this.hide = !this.hide;
+  }
+
+  inputType(): string{
+    return this.hide ? 'password' : 'text';
+  }
+
+  login(){
+    this.userService.login(this.userLogin).subscribe(
+      response => {
+        this.authService.setUserInfo(response)
+      }
+    )
+  }
+
+  public userLogin: UserLogin =
+  {
+    username:'',
+    password:''
   }
 
 }
--- frontend/src/app/user/user.service.ts
+++ frontend/src/app/user/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { UserLogin } from './user-login';
+import { UserAuth } from './user-auth';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private httpCliente: HttpClient,
+
+  ) { }
+  
+  errorHandler(e: any): Observable<any> {
+    return EMPTY;
+  }
+
+  login(userCredentials: UserLogin): Observable<UserAuth> {
+    return this.httpCliente.post<UserAuth>(`http://localhost:8000/login/`, userCredentials).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    )
+  }
 }
+
