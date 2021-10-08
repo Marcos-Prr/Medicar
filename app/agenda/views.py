cc82d861bfde3fa52462d36dcb3ce1308fb04f0c
MarcosPrr <marcosprr34@gmail.com>
2021-10-08 16:36:54 -0300

    inclusão de filtro de agenda no backend

--- app/agenda/views.py
+++ app/agenda/views.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from rest_framework import viewsets
 from rest_framework import views
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Agenda ,Horario
 from .serializers import agendaSerializer,horarioSerializer
 
@@ -11,6 +12,8 @@ class AgendaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Agenda.objects.all()
     serializer_class = agendaSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['medico']
 
     horarios_livres = Horario.objects.disponivel().filter(agenda=OuterRef('pk'), marcado = False)
     queryset = Agenda.objects.disponivel().prefetch_related(Prefetch('horarios',queryset=Horario.objects.disponivel())).filter(Exists(horarios_livres))
