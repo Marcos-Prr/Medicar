edb80af56685317770098ce983a4f71575d33125
MarcosPrr <marcosprr34@gmail.com>
2021-10-08 15:17:00 -0300

    mudança de título e adição de css para dialog de deleção

--- frontend/src/app/consulta/delete/delete.component.ts
+++ frontend/src/app/consulta/delete/delete.component.ts
@@ -5,6 +5,7 @@ import { ConsultaService } from '../consulta.service';
 @Component({
   selector: 'app-delete',
   templateUrl: 'delete.component.html',
+  styleUrls: ['delete.component.css']
 })
 export class DeleteComponent implements OnInit {
 
